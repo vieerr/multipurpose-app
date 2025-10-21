f59e832d79696c58d1bb3b71121ad4f111934af7
vieerr <olivier_paspuel@hotmail.com>
2025-10-21 10:41:29 -0500

    ci(config): 🎡 add commitzen initial config

---
+++ changelog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
+  scopes: [
+    'coins',
+    'diet',
+    'perfect',
+    'POS',
+    'years',
+    'config',
+    'home'
+  ],
+  maxMessageLength: 100,
+}
