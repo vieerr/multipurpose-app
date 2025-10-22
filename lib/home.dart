e6260d749309aa37bee2d4b7bc34fb5e6d88e8e3
vieerr <olivier_paspuel@hotmail.com>
2025-10-22 10:16:58 -0500

    feat(home): 🎸 add main home with navigation bar

---
+++ lib/home.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Super App",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        elevation: 4,
+        shadowColor: Colors.black26,
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onPressed: () {
+            // Add menu functionality here if needed
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications, color: Colors.white),
+            onPressed: () {
+              // Add notifications functionality here if needed
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: _bottomNavigationBar(),
+      body: <Widget>[
+        Card(child: Center(child: Text("Inicio"))),
+      ][currentPageIndex],
+    );
+  }
+
+  NavigationBar _bottomNavigationBar() {
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+      },
+      // indicatorColor: Colors.blueAccent,
+      selectedIndex: currentPageIndex,
+      destinations: const <Widget>[
+        NavigationDestination(icon: Icon(Icons.shop), label: "Cajero"),
+        NavigationDestination(
+          icon: Icon(Icons.monetization_on),
+          label: "Monedas",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.calendar_month),
+          label: "Bisiesto",
+        ),
+        NavigationDestination(icon: Icon(Icons.check), label: "Perfecto"),
+        NavigationDestination(icon: Icon(Icons.food_bank), label: "Dieta"),
+      ],
+    );
+  }
+}
